d27cbc598165cfddee54ca78dc3aa715a70eb9c4
Prachi Pandey <115707069+prachipandey16@users.noreply.github.com>
2024-01-24 14:06:54 +0530

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,32 @@
 import streamlit as st
 
-st.write("Hello, My name is Prachi Pandey")
+import os
+import openai
+from openai import OpenAI
+
+st.title("Text generation Using AI")
+client = OpenAI(api_key='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')
+
+def Messages(query):
+    response = client.completions.create(
+    model="gpt-3.5-turbo-instruct",
+    prompt=query,
+    temperature=0.7,
+    max_tokens=300,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    #print(response)
+    #if 'choices' in response: 
+        #if (len(response.choices)>0):
+    answer = response.choices[0].text
+  return answer
+    #print(answer)
+
+query = st.text_input("Enter something in prompt : ")
+
+if st.button("Get response"):
+  response = Messages(query)
+  st.text("Model response")
+  st.write(response)
